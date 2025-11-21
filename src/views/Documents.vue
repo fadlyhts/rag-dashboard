48fc76a0ac17a96da59b475a14f56dfe5d22b82b
fadlyhts <fadlyhutasuhut1305@gmail.com>
2025-11-21 10:19:33 +0700

    feat: add initial Documents view with placeholder content

---
+++ src/views/Documents.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold text-gray-900 mb-4">Documents</h1>
+    <p class="text-gray-600">Document management page - Coming soon</p>
+  </div>
+</template>
