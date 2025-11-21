fc84c80cdb41b7b049e7d671581f3550698c68b3
fadlyhts <fadlyhutasuhut1305@gmail.com>
2025-11-21 10:21:54 +0700

    feat: initialize Vue application with main entry point and setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { Toaster } from 'vue-sonner'
+</script>
+
+<template>
+  <div>
+    <Toaster position="top-right" :duration="3000" rich-colors />
+    <RouterView />
+  </div>
+</template>
+
