68d72f02a8128d50d13adeefa699bb9725202a3e
fadlyhts <fadlyhutasuhut1305@gmail.com>
2025-11-21 10:17:23 +0700

    feat: add utility functions for class name merging and value updating

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+import type { ClassValue } from "clsx"
+import type { Ref } from "vue"
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function valueUpdater<T>(updaterOrValue: T | ((old: T) => T), ref: Ref<T>) {
+  ref.value
+    = typeof updaterOrValue === "function"
+      ? (updaterOrValue as (old: T) => T)(ref.value)
+      : updaterOrValue
+}
